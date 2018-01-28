13248c5b8a25fe442cbf7146a9dd22568ac61d09
Sang Nguyen <qsang-12@hotmail.com>
2018-01-28 17:44:19 +0100

    Added OvercrowdedView.java

---
+++ src/Parkeersimulator/view/OvercrowdedView.java
@@ -0,0 +1,57 @@
+package Parkeersimulator.view;
+
+
+import Parkeersimulator.logic.Model;
+
+import java.awt.*;
+
+public class OvercrowdedView extends AbstractView {
+
+    private int aantalExitCars;
+    private int aantalQueueCars;
+
+
+    private int maxAantalQueue;
+
+    public OvercrowdedView(Model Model) {
+        super(Model);
+        setSize(300,300);
+    }
+
+    public int calculateDegrees(double aantalCars)
+    {
+        int degrees;
+        double garageSize = 540;
+
+        degrees = (int) (((100 / garageSize) * aantalCars) * 3.6);
+
+        return degrees;
+
+    }
+
+    protected void paintComponent(Graphics a) {
+        super.paintComponent(a);
+
+        aantalQueueCars = model.getEntranceCarQueue() + model.getPassCarQueue();
+        maxAantalQueue = 5;
+
+        if(maxAantalQueue < aantalQueueCars){
+            aantalExitCars = aantalQueueCars -2;
+        }
+
+
+        System.out.println("AantalQueueCars :" + aantalQueueCars + " AantalExitCars:" + aantalExitCars);
+
+
+        a.setColor(Color.white);
+        a.fillArc(15, 15, 250, 250, 350, 360);
+
+        a.setColor(Color.blue);
+        a.fillArc(15, 15, 250, 250, 0, calculateDegrees(aantalQueueCars));
+        a.setColor(Color.green);
+        a.fillArc(15, 15, 250, 250, calculateDegrees(aantalQueueCars), calculateDegrees(aantalExitCars));
+
+
+
+    }
+}
